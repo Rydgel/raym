c8bba41ffcff682dad0f8ea622e387fa8630f892
Jérôme Mahuet <jerome.mahuet@gmail.com>
2025-01-26 23:50:37 +0100

    remove shit

--- src/perlin.h
+++
@@ -1,8 +0,0 @@
-#ifndef PERLIN_H
-#define PERLIN_H
-
-float GetPerlinValue(float x, float y, float z);
-float GetSimplexValue(float x, float y, float z);
-float GetSimplexFractalValue(float x, float y, float z, int octaves, float persistence);
-
-#endif // PERLIN_H
